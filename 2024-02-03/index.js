aa1554b3c134fadb51da294188d609f05cf530d8
angel <angel@elevateclub.org>
2024-02-03 14:31:25 -0500

    functions made

--- 2024-02-03/index.js
+++ 2024-02-03/index.js
@@ -4,12 +4,23 @@ var ox = 0;
 var oy = 0;
 var pox = ox+50;
 var poy = oy+60;
-ctx.beginPath();
-//frame
-ctx.rect(ox, oy, 100, 200);
-//tmino
-ctx.rect(pox, poy, 10, 10);
-ctx.rect(pox+10, poy, 10, 10);
-ctx.rect(pox+20, poy, 10, 10);
-ctx.rect(pox+10, poy+10, 10, 10);
-ctx.stroke();
+
+function frame(ox, oy) {
+    ctx.beginPath();
+    //frame
+    ctx.rect(ox, oy, 100, 200);
+    ctx.stroke();
+}
+
+function tmino(pox, poy) {
+    //tmino
+    ctx.beginPath();
+    ctx.rect(pox, poy, 10, 10);
+    ctx.rect(pox+10, poy, 10, 10);
+    ctx.rect(pox+20, poy, 10, 10);
+    ctx.rect(pox+10, poy+10, 10, 10);
+    ctx.stroke();
+}
+
+frame (ox,oy);
+tmino (pox, poy);
