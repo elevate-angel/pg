3e34c0d8ab32feafdfa1b98529bb53fecb1d8140
angel <angel@elevateclub.org>
2024-02-24 13:38:47 -0500

    all minos

--- 2024-02-03/index.js
+++ 2024-02-03/index.js
@@ -23,10 +23,70 @@ function tmino(pox, poy) {
     ctx.stroke();
 }
 
+function imino(pox, poy) {
+    //imino
+    ctx.beginPath();
+    ctx.rect(pox, poy, s, s);
+    ctx.rect(pox + s, poy , s, s);
+    ctx.rect(pox + s * 2, poy , s, s);
+    ctx.rect(pox + s * 3, poy , s, s);
+    ctx.stroke();
+}
+
+function omino(pox, poy) {
+    //omino
+    ctx.beginPath();
+    ctx.rect(pox, poy, s, s);
+    ctx.rect(pox + s, poy , s, s);
+    ctx.rect(pox, poy + s, s, s);
+    ctx.rect(pox + s, poy + s, s, s);
+    ctx.stroke();
+}
+
+function lmino(pox, poy) {
+    //lmino
+    ctx.beginPath();
+    ctx.rect(pox, poy, s, s);
+    ctx.rect(pox + s, poy , s, s);
+    ctx.rect(pox + s * 2, poy , s, s);
+    ctx.rect(pox , poy + s, s, s);
+    ctx.stroke();
+}
+
+function jmino(pox, poy) {
+    //jmino
+    ctx.beginPath();
+    ctx.rect(pox, poy, s, s);
+    ctx.rect(pox + s, poy , s, s);
+    ctx.rect(pox + s * 2, poy , s, s);
+    ctx.rect(pox + s * 2, poy + s, s, s);
+    ctx.stroke();
+}
+
+function zmino(pox, poy) {
+    //zmino
+    ctx.beginPath();
+    ctx.rect(pox, poy, s, s);
+    ctx.rect(pox + s, poy , s, s);
+    ctx.rect(pox + s , poy + s, s, s);
+    ctx.rect(pox + s * 2, poy + s, s, s);
+    ctx.stroke();
+}
+
+function smino(pox, poy) {
+    //smino
+    ctx.beginPath();
+    ctx.rect(pox + s * 2, poy, s, s);
+    ctx.rect(pox + s, poy , s, s);
+    ctx.rect(pox + s , poy + s, s, s);
+    ctx.rect(pox, poy + s, s, s);
+    ctx.stroke();
+}
+
 function draw(ts){
     ctx.clearRect(0,0,800,600);
     frame (ox,oy);
-    tmino (ox,oy);
+    smino (ox,oy);
 }
 
 function run(ts){
